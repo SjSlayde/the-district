f7bff00ce10f710d70af435e8e2ebc4054fb315a
houbron <benjamin.houbrongmail.com>
2024-04-16 15:40:42 +0200

    fuck

---
+++ js/filtre.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+
+form.addEventListener('submit', function (e) {
+e.preventDefault();
+let checkvalide = true;
+checkvalide &= verifiltre(/^[A-Za-z]+$/,document.getElementById("Nom"),"Nom invalide")
+checkvalide &= verifiltre(/^[A-Za-z]+$/,document.getElementById("Prenom"),"Prénom invalide")
+checkvalide &= verifiltre(/^[_A-Za-z0-9.-]+@[_a-z0-9.-]+.[a-z]{2,4}$/,document.getElementById("Email"),"veuillez entrer une adresse email valide")
+checkvalide &= verifiltre(/^[0-9{10}]+$/,document.getElementById("Tel"),"numéros de télèphone invalide")
+checkvalide &= verifiltre(/^[A-Za-z]+$/,document.getElementById("Demande"),"veuillez ecrire votre question")
+console.log(checkvalide)
+console.log(check)
+if(checkvalide){
+    form.submit();
+    console.log("ça passe")
+}
+})
+})
+
+function verifiltre(regex,element,mde){
+    if(!regex.test(element.value)){
+        alert(mde);
+        element.focus();
+        return false;
+    }
+    return true;
+}
